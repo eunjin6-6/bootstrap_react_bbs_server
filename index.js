95d8f9a58c176fbe432f12e9f0f6cbb7029709f9
eunjin6-6 <dmswls616@gmail.com>
2024-11-04 16:55:17 +0900

    express 파싱 설정

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+//본문을 통해서 넘어온 요청 파싱(=변환)하기 위해 설치한 미들웨어 (body-parser)를 이용
+app.use(express.json()); //jason 형식으로 변환 //{"name":"Alice", "age":"25"}
+app.use(express.urlencoded()); //json -> object로 변환 {name:"Alice", age: '29'}
+
 var corsOptions = {
   //origin: 'http://localhost:3000',
   origin: '*' //모든 출처 허용
@@ -44,6 +48,18 @@ app.get('/list', (req, res) => {
   });
 })
 
+
+app.post('/insert', (req, res) => {
+  //console.log(req.body.title); //post 방식이라 body 안에 숨어서옴
+  let title = req.body.title;
+  let content = req.body.content;
+
+  const sql = "INSERT INTO board (BOARD_TITLE, BOARD_CONTENT, REGISTER_ID) VALUES (?, ?, 'admin')"; //보안상 아래 작성
+  db.query(sql, [title, content], (err, result) => {
+    if (err) throw err;
+    res.send(result);
+  });
+})
 
 
 
